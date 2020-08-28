52ca78fbd44262657fcb97fff6b705fd25ad16d3
NataljaB <natalja.buljko@gmail.com>
2020-08-28 15:29:06 +0300

    Initial Commit

---
+++ TestingGitProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingGitProject
+//
+//  Created by natalja.buljko on 28/08/2020.
+//  Copyright © 2020 natalja.buljko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
